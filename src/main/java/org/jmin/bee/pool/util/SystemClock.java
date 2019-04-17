ca041c43344a88815f11572b701d0733328a346c
Chris <32663325+Chris2018998@users.noreply.github.com>
2019-04-17 21:09:17 +0800

    Add files via upload

--- src/main/java/org/jmin/bee/pool/util/SystemClock.java
+++ src/main/java/org/jmin/bee/pool/util/SystemClock.java
@@ -1,27 +1,53 @@
 package org.jmin.bee.pool.util;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 /**
- * System Clock
+ * Time Clock
  * 
  * @author Chris.liao
  */
-public class SystemClock extends TimerTask {
-	private volatile long currentTimeMillis = 0;
-	private static final SystemClock CLOCK = new SystemClock(1);
+public class SystemClock implements Runnable {
+	private volatile long nowTimeMillis = System.currentTimeMillis();
+	public static final SystemClock clock = new SystemClock(1);
 
 	private SystemClock(long period) {
-		this.currentTimeMillis = System.currentTimeMillis();
-		new Timer(true).schedule(this, 1, period);
+		ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1, new ThreadFactory() {
+			public Thread newThread(Runnable r) {
+				Thread thread = new Thread(r, "System Clock");
+				thread.setDaemon(true);
+				return thread;
+			}
+		});
+		scheduler.scheduleAtFixedRate(this, period, period, TimeUnit.MILLISECONDS);
 	}
-
 	public void run() {
-		this.currentTimeMillis = System.currentTimeMillis();
+		this.nowTimeMillis = System.currentTimeMillis();
 	}
-
+	public static long nanoTime() {
+		return System.nanoTime();
+	}
 	public static long currentTimeMillis() {
-		return CLOCK.currentTimeMillis;
+		return clock.nowTimeMillis;
+	}
+	public static void main(String[] args) {
+		long time1= System.nanoTime();
+		for (int i = 0; i < 1000; i++) {
+			SystemClock.currentTimeMillis();
+		}
+		long time2= System.nanoTime();
+		
+		
+		long time3= System.nanoTime();
+		for (int i = 0; i < 1000; i++) {
+			System.currentTimeMillis();
+		}
+		long time4= System.nanoTime();
+		
+		
+		System.out.println("s1:" + (time2-time1));
+		System.out.println("t2:" + (time4-time3));
 	}
 }
