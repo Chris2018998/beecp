1b88ca65e78c7abb0e429df577d8d26ba12b3641
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2019-08-19 13:26:57 +0800

    Add files via upload

--- src/main/java/org/jmin/bee/BeeDataSourceFactory.java
+++ src/main/java/org/jmin/bee/BeeDataSourceFactory.java
@@ -15,9 +15,12 @@
  */
 package org.jmin.bee;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Hashtable;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -74,7 +77,7 @@ public final class BeeDataSourceFactory implements ObjectFactory {
 	}
 	public DataSource lookup(String jndi) throws NamingException {
 		InitialContext ctx = new InitialContext(initProperties);
-		return (DataSource) ctx.lookup(jndi);
+		return new JndiDataSourceWrapper((DataSource) ctx.lookup(jndi));
 	}
 	public void bind(String jndi, DataSource obj) throws NamingException {
 		InitialContext ctx = new InitialContext(initProperties);
@@ -176,5 +179,40 @@ public final class BeeDataSourceFactory implements ObjectFactory {
 	
 	private boolean isNull(String value) {
 		return (value == null || value.trim().length()==0);
+	}
+	
+	class JndiDataSourceWrapper implements DataSource {
+		private DataSource delegete;
+		public JndiDataSourceWrapper(DataSource delegete) {
+			this.delegete = delegete;
+		}
+		public Connection getConnection() throws SQLException {
+			return delegete.getConnection();
+		}
+		public Connection getConnection(String username, String password) throws SQLException {
+			return delegete.getConnection(username, password);
+		}
+		public java.io.PrintWriter getLogWriter() throws SQLException {
+			return delegete.getLogWriter();
+		}
+		public void setLogWriter(java.io.PrintWriter out) throws SQLException {
+			delegete.setLogWriter(out);
+		}
+		public void setLoginTimeout(int seconds) throws SQLException {
+			delegete.setLoginTimeout(seconds);
+		}
+		public int getLoginTimeout() throws SQLException {
+			return delegete.getLoginTimeout();
+		}
+		public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+			return null;
+			//return delegete.getParentLogger();
+		}
+		public <T> T unwrap(java.lang.Class<T> iface) throws java.sql.SQLException {
+			return delegete.unwrap(iface);
+		}
+		public boolean isWrapperFor(java.lang.Class<?> iface) throws java.sql.SQLException {
+			return delegete.isWrapperFor(iface);
+		}
 	}
 }
