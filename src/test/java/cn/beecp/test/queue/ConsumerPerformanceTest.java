6cc6d1e6229405103890423eb2ea1a86340a1ee7
欧德 <32663325+Chris2018998@users.noreply.github.com>
2020-07-19 19:52:32 +0800

    Add files via upload

--- src/test/java/cn/beecp/test/queue/ConsumerPerformanceTest.java
+++ src/test/java/cn/beecp/test/queue/ConsumerPerformanceTest.java
@@ -1,1 +1,144 @@
+/*
+ * Copyright Chris2018998
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.beecp.test.queue;
+
+import cn.beecp.util.ConcurrentTransferQueue;
+
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Consumer Time PerformanceTest
+ *
+ * @author Chris.Liao
+ */
+public class ConsumerPerformanceTest {
+    public static void main(String[] args) throws Exception {
+        int producerSize = 10, consumerSize = 100, takeTimes = 100;
+        testTransferQueue("ArrayBlockingQueue", new ArrayBlockingQueue<Object>(1000), producerSize, consumerSize, takeTimes);
+        testTransferQueue("LinkedBlockingQueue",new LinkedBlockingQueue<Object>(), producerSize, consumerSize, takeTimes);
+        testTransferQueue("LinkedTransferQueue",new LinkedTransferQueue<Object>(), producerSize, consumerSize, takeTimes);
+        testTransferQueue("ConcurrentTransferQueue",new ConcurrentTransferQueue<Object>(), producerSize, consumerSize, takeTimes);
+    }
+
+    private static void testTransferQueue(String queueName, Queue<Object> queue, int producerSize, int consumerSize, int pollSize) throws Exception {
+        Consumer[] consumers = new Consumer[consumerSize];
+        CountDownLatch producersDownLatch = new CountDownLatch(producerSize);
+        CountDownLatch consumersDownLatch = new CountDownLatch(consumerSize);
+        AtomicBoolean existConsumerInd= new AtomicBoolean(true);
+        Method pollMethod=queue.getClass().getMethod("poll",new Class[]{Long.TYPE,TimeUnit.class});
+        long startTime = System.nanoTime() + TimeUnit.SECONDS.toNanos(10);
+
+        //Consumers
+        for (int i = 0; i < consumerSize; i++) {
+            consumers[i] = new Consumer(queue,startTime,pollSize,consumersDownLatch,pollMethod);
+            consumers[i].start();
+        }
+
+        //Producers
+        for (int i = 0; i < producerSize; i++) {
+            new Producer(queue,startTime,existConsumerInd,producersDownLatch).start();
+        }
+
+        consumersDownLatch.await();
+        existConsumerInd.set(false);
+        producersDownLatch.await();
+
+        //Summary and Conclusion
+        int totalExeSize=consumerSize*pollSize;
+        BigDecimal totTime = new BigDecimal(0);
+        for (int i = 0; i < consumerSize; i++) {
+            totTime=totTime.add(new BigDecimal(consumers[i].getTookTime()));
+        }
+
+        if(totTime.longValue()>0) {
+            BigDecimal avgTime = totTime.divide(new BigDecimal(totalExeSize),0, BigDecimal.ROUND_HALF_UP);
+            System.out.println("<" + queueName + "> producer-size:" + producerSize + ",consumer-size:"
+                    + consumerSize + ",poll total count:" + totalExeSize + ",total time:" + totTime.longValue()
+                    + "(ns),avg time:" + avgTime + "(ns)");
+        }
+    }
+
+    private static final Object transferObject=new Object();
+    static final class Producer extends Thread {
+        private long startTime;
+        private Queue<Object> queue;
+        private AtomicBoolean activeInd;
+        private CountDownLatch producersDownLatch;
+
+        public Producer(Queue<Object> queue,long startTime,AtomicBoolean activeInd, CountDownLatch producersDownLatch) {
+            this.queue = queue;
+            this.startTime=startTime;
+            this.activeInd = activeInd;
+            this.producersDownLatch=producersDownLatch;
+        }
+        public void run() {
+            while (activeInd.get()) {
+                queue.offer(transferObject);
+            }
+            producersDownLatch.countDown();
+        }
+    }
+
+    static final class Consumer extends Thread {
+        private long startTime;
+        private int loopTimes;
+        private CountDownLatch latch;
+        private Method pollMethod;
+        private Queue<Object>queue;
+        private long tookTime;
+        public long getTookTime(){
+            return tookTime;
+        }
+        public Consumer(Queue<Object> queue,long startTime,int pollTimes, CountDownLatch latch, Method pollMethod) {
+            this.queue = queue;
+            this.startTime=startTime;
+            this.loopTimes = pollTimes;
+            this.latch = latch;
+            this.pollMethod=pollMethod;
+        }
+        public void run() {
+            LockSupport.parkNanos(startTime - System.nanoTime());
+            long time1=System.nanoTime();
+            for (int i = 0; i < loopTimes;i++) {
+                 poll(queue,pollMethod);
+            }
+            tookTime=System.nanoTime()-time1;
+            latch.countDown();
+        }
+    }
+    private static final Object poll(Queue<Object>queue,Method pollMethod){
+        try {
+            return  pollMethod.invoke(queue, new Object[]{Integer.MAX_VALUE, TimeUnit.NANOSECONDS});
+        }catch(Throwable e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
+
+
 
---
+++ src/test/java/cn/beecp/test/queue/TransferPacketPerformanceTest.java
@@ -0,0 +1,134 @@
+package cn.beecp.test.queue;
+
+import cn.beecp.util.ConcurrentTransferQueue;
+
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Transfer Packet Performance Test
+ *
+ * @author Chris.Liao
+ */
+public class TransferPacketPerformanceTest {
+    public static void main(String[] args) throws Exception {
+        int producerSize = 10, consumerSize = 100, takeTimes = 100;
+         testTransferQueue("ArrayBlockingQueue", new ArrayBlockingQueue<TransferPacket>(1000), producerSize, consumerSize, takeTimes);
+         testTransferQueue("LinkedBlockingQueue",new LinkedBlockingQueue<TransferPacket>(), producerSize, consumerSize, takeTimes);
+         testTransferQueue("LinkedTransferQueue",new LinkedTransferQueue<TransferPacket>(), producerSize, consumerSize, takeTimes);
+         testTransferQueue("ConcurrentTransferQueue",new ConcurrentTransferQueue<TransferPacket>(), producerSize, consumerSize, takeTimes);
+    }
+
+    private static void testTransferQueue(String queueName,Queue<TransferPacket> queue, int producerSize,int consumerSize,int pollSize) throws Exception {
+        Consumer[] consumers = new Consumer[consumerSize];
+        CountDownLatch producersDownLatch = new CountDownLatch(producerSize);
+        CountDownLatch consumersDownLatch = new CountDownLatch(consumerSize);
+        AtomicBoolean existConsumerInd= new AtomicBoolean(true);
+        Method pollMethod=queue.getClass().getMethod("poll",new Class[]{Long.TYPE,TimeUnit.class});
+
+        //Consumers
+        for (int i = 0; i < consumerSize; i++) {
+            consumers[i] = new Consumer(queue,pollSize,consumersDownLatch,pollMethod);
+            consumers[i].start();
+        }
+
+        //Producers
+        long startTime = System.nanoTime() + TimeUnit.SECONDS.toNanos(10);
+        for (int i = 0; i < producerSize; i++) {
+             new Producer(queue, startTime, existConsumerInd,producersDownLatch).start();
+        }
+
+        consumersDownLatch.await();
+        existConsumerInd.set(false);
+        producersDownLatch.await();
+
+        //Summary and Conclusion
+        int totPacketSize=0;
+        BigDecimal totTime = new BigDecimal(0);
+        for (int i = 0; i < consumerSize; i++) {
+           for(TransferPacket packet:consumers[i].getPacketList()){
+               totPacketSize++;
+               totTime=totTime.add(new BigDecimal(packet.arriveTime-packet.sendTime));
+           }
+        }
+        if(totTime.longValue() >0 && totPacketSize>0) {
+            BigDecimal avgTime = totTime.divide(new BigDecimal(totPacketSize),0, BigDecimal.ROUND_HALF_UP);
+            System.out.println("<" + queueName + "> producer-size:" + producerSize + ",consumer-size:"
+                    + consumerSize + ",total packet size:" + totPacketSize + ",total time:" + totTime.longValue()
+                    + "(ns),avg time:" + avgTime + "(ns)");
+        }
+    }
+
+    static final class TransferPacket {
+        long sendTime = System.nanoTime();
+        long arriveTime;
+    }
+
+    static final class Producer extends Thread {
+        private long startTime;
+        private AtomicBoolean activeInd;
+        private Queue<TransferPacket> queue;
+        private CountDownLatch producersDownLatch;
+
+        public Producer(Queue<TransferPacket> queue,long startTime, AtomicBoolean activeInd,CountDownLatch producersDownLatch) {
+            this.queue = queue;
+            this.startTime=startTime;
+            this.activeInd = activeInd;
+            this.producersDownLatch=producersDownLatch;
+        }
+        public void run() {
+            LockSupport.parkNanos(startTime - System.nanoTime());
+            while (activeInd.get()) {
+                queue.offer(new TransferPacket());
+            }
+            producersDownLatch.countDown();
+        }
+    }
+
+    static final class Consumer extends Thread {
+        private int loopTimes;
+        private CountDownLatch latch;
+        private Method pollMethod;
+        private Queue<TransferPacket>queue;
+        private List<TransferPacket> packetList=new LinkedList();
+        public List<TransferPacket> getPacketList() {
+            return packetList;
+        }
+        public Consumer(Queue<TransferPacket> queue, int pollTimes, CountDownLatch latch,Method pollMethod) {
+            this.queue = queue;
+            this.loopTimes = pollTimes;
+            this.latch = latch;
+            this.pollMethod=pollMethod;
+        }
+
+        public void run() {
+            for (int i = 0; i < loopTimes;i++) {
+                TransferPacket packet=poll(queue,pollMethod);
+                if(packet!=null) {
+                    packet.arriveTime = System.nanoTime();
+                    packetList.add(packet);
+                }
+            }
+            latch.countDown();
+        }
+    }
+    private static final TransferPacket poll(Queue<TransferPacket>queue,Method pollMethod){
+        try {
+            return (TransferPacket) pollMethod.invoke(queue, new Object[]{Integer.MAX_VALUE, TimeUnit.NANOSECONDS});
+        }catch(Throwable e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
