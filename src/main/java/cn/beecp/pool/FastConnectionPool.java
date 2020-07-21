541eae2db0c781169d2199d825d634ae42257241
欧德 <32663325+Chris2018998@users.noreply.github.com>
2020-07-21 21:08:31 +0800

    Add files via upload

--- src/main/java/cn/beecp/pool/FastConnectionPool.java
+++ src/main/java/cn/beecp/pool/FastConnectionPool.java
@@ -448,26 +448,26 @@ public final class FastConnectionPool extends Thread implements ConnectionPool, 
 
                                 borrower.stateObject = PoolObjectsState.BORROWER_NORMAL;
                                 yield();
-                            } else {
-                                if (stateObject instanceof SQLException)
-                                    throw (SQLException)stateObject;
+                            }
 
-								if (isInterrupted){
-									if(BorrowerStateUpdater.compareAndSet(borrower,stateObject,BORROWER_INTERRUPTED))
-										throw RequestInterruptException;
-									continue;
-								}
+							if (stateObject instanceof SQLException)
+								throw (SQLException)stateObject;
 
-								if (isNotTimeout && (isNotTimeout = (timeout = deadline - nanoTime()) > 0L)) {
-									if (spinSize > 0) {
-										spinSize--;
-									} else if (timeout>spinForTimeoutThreshold && BorrowerStateUpdater.compareAndSet(borrower, stateObject,BORROWER_WAITING)) {
-										LockSupport.parkNanos(this,timeout);
-										isInterrupted=borrowThread.isInterrupted();
-									}
-								}else if(BorrowerStateUpdater.compareAndSet(borrower,stateObject,BORROWER_TIMEOUT)) {
-									throw RequestTimeoutException;
+							if (isInterrupted){
+								if(BorrowerStateUpdater.compareAndSet(borrower,stateObject,BORROWER_INTERRUPTED))
+									throw RequestInterruptException;
+								continue;
+							}
+
+							if (isNotTimeout && (isNotTimeout = (timeout = deadline - nanoTime()) > 0L)) {
+								if (spinSize > 0) {
+									spinSize--;
+								} else if (timeout>spinForTimeoutThreshold && BorrowerStateUpdater.compareAndSet(borrower, stateObject,BORROWER_WAITING)) {
+									LockSupport.parkNanos(this,timeout);
+									isInterrupted=borrowThread.isInterrupted();
 								}
+							}else if(BorrowerStateUpdater.compareAndSet(borrower,stateObject,BORROWER_TIMEOUT)) {
+								throw RequestTimeoutException;
 							}
                         }//while
                     } finally {
