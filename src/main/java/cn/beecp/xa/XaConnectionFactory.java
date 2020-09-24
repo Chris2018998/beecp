65d0438c1e6a5e949538fba7e9514004c66d12d7
欧德 <32663325+Chris2018998@users.noreply.github.com>
2020-09-24 21:43:17 +0800

    Add files via upload
    增加XA支持

--- src/main/java/cn/beecp/xa/XaConnectionFactory.java
+++ src/main/java/cn/beecp/xa/XaConnectionFactory.java
@@ -1,1 +1,39 @@
+/*
+ * Copyright Chris2018998
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.beecp.xa;
 
+import javax.sql.XAConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * XAConnection Factory
+ *
+ * @author Chris.Liao
+ * @version 1.0
+ */
+public interface XaConnectionFactory {
+
+    /**
+     * Create XAConnection instance
+     *
+     * @param rawCon raw connection from pool
+     * @return XAConnection
+     * @throws SQLException if failed then throw SQLException
+     */
+    public XAConnection create(Connection rawCon) throws SQLException;
+
+}
