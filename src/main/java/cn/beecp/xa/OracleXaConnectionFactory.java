9cb9be6384009df1f520679de1b39e3daddeec44
欧德 <32663325+Chris2018998@users.noreply.github.com>
2020-09-24 22:54:16 +0800

    Add files via upload

--- src/main/java/cn/beecp/xa/OracleXaConnectionFactory.java
+++ src/main/java/cn/beecp/xa/OracleXaConnectionFactory.java
@@ -25,7 +25,7 @@ import java.sql.SQLException;
  *  @author Chris Liao
  *  @version 1.0
  */
-public class OracleXaConnection implements XaConnectionFactory{
+public class OracleXaConnectionFactory implements XaConnectionFactory{
 
     /**
      * Create XAConnection instance
