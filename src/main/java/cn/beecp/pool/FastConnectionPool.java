f9e5db80f52ac349b9d89682fb2b6451ddc51e8b
欧德 <32663325+Chris2018998@users.noreply.github.com>
2020-10-25 11:07:55 +0800

    Update FastConnectionPool.java

--- src/main/java/cn/beecp/pool/FastConnectionPool.java
+++ src/main/java/cn/beecp/pool/FastConnectionPool.java
@@ -419,7 +419,7 @@ public final class FastConnectionPool extends Thread implements ConnectionPool, 
                     if (timeout > 0L) {
                         if (spinSize > 0) {
                             --spinSize;
-                        } else if (timeout > spinForTimeoutThreshold && BwrStUpd.compareAndSet(borrower, state, BORROWER_WAITING)) {
+                        } else if (timeout-spinForTimeoutThreshold>0 && BwrStUpd.compareAndSet(borrower, state, BORROWER_WAITING)) {
                             parkNanos(borrower, timeout);
                             BwrStUpd.compareAndSet(borrower, BORROWER_WAITING, BORROWER_NORMAL);//reset to normal
                             if (borrower.thread.isInterrupted()) {
