70bcf5f0a5ff12d0eada93f4e922fb8f0cebd68b
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-02-16 23:31:43 +0800

    Add files via upload

---
+++ src/main/java/cn/beecp/PasswordDecoder.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright(C) Chris2018998
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package cn.beecp;
+
+/**
+ * Password decoder
+ *
+ * @author Chris.Liao
+ * @version 1.0
+ */
+public class PasswordDecoder {
+
+    public String decode(String password) {
+        return password;
+    }
+}
