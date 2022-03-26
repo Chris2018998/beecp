770ed2bbbf5ca8489b66f0bd3cdf5da2bed69d26
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-03-26 14:29:01 +0800

    Add files via upload

--- src/test/java/cn/beecp/test/mock/MockConnection.java
+++ src/test/java/cn/beecp/test/mock/MockConnection.java
@@ -21,11 +21,7 @@ public class MockConnection extends MockBase implements Connection {
     private boolean autoCommit;
     private int holdability;
     private String schema;
-
-    public MockConnection() {
-        //do nothing
-    }
-
+    
     public Statement createStatement() throws SQLException {
         return new MockStatement(this);
     }
