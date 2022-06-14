52218c7da04be613b90b32221174b47a9692663e
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-06-14 22:52:57 +0800

    Add files via upload

--- src/test/java/cn/beecp/test/other/MysqlAutoChangeTest.java
+++ src/test/java/cn/beecp/test/other/MysqlAutoChangeTest.java
@@ -14,7 +14,8 @@ public class MysqlAutoChangeTest {
         String password="";
 
         Connection con1=null;
-        Statement statement1=null;
+        Statement statement10=null;
+        Statement statement11=null;
 
         Connection con2=null;
         Statement statement2=null;
@@ -25,13 +26,14 @@ public class MysqlAutoChangeTest {
             con1= DriverManager.getConnection(url,user,password);
 
             //2：先清除表中所有数据
-            statement1=con1.createStatement();
-            statement1.execute("delete from test");
+            statement10=con1.createStatement();
+            statement10.execute("delete from test");
 
             //3: AutoCommit 由false 切换为 true
             con1.setAutoCommit(false);
             String testValue="123456";
-            statement1.execute("insert into test values("+testValue+")");
+            statement11=con1.createStatement();
+            statement11.execute("insert into test values("+testValue+")");
             con1.setAutoCommit(true);//<----会触发自动提交，不建议随意切换
 
             //4:建立一个新连接，检查第3步是否已经提交到库里
@@ -41,11 +43,12 @@ public class MysqlAutoChangeTest {
             while(resultSet2.next())
                System.out.println(resultSet2.getString("id"));
         }finally {
-            if(statement1!=null)statement1.close();
+            if(statement10!=null)statement10.close();
+            if(statement11!=null)statement11.close();
+            if(con1!=null)con1.close();
+
             if(resultSet2!=null)resultSet2.close();
             if(statement2!=null)statement2.close();
-
-            if(con1!=null)con1.close();
             if(con2!=null)con2.close();
         }
     }
