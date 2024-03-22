6e468569c11c2f5c76d9f64b50375d06186a5ca4
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-03-22 19:10:07 +0800

    update testcase file

--- src/test/java/org/stone/beecp/config/ConfigLoadFailTest.java
+++ src/test/java/org/stone/beecp/config/ConfigLoadFailTest.java
@@ -10,10 +10,33 @@
 package org.stone.beecp.config;
 
 import junit.framework.TestCase;
+import org.stone.beecp.BeeDataSourceConfig;
+
+import java.io.File;
 
 public class ConfigLoadFailTest extends TestCase {
 
     public void test() throws Exception {
+        BeeDataSourceConfig config = new BeeDataSourceConfig();
+        try {
+            config.loadFromProperties(null);
+        } catch (IllegalArgumentException e) {
+            //do noting
+        }
+        try {
+            config.loadFromPropertiesFile((File) null);
+        } catch (IllegalArgumentException e) {
+            //do noting
+        }
+
+        try {
+            config.loadFromPropertiesFile((String) null);
+        } catch (IllegalArgumentException e) {
+            //do noting
+        }
+
+
+
 
     }
 }
