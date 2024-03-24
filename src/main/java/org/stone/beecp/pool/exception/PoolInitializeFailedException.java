6dd862b1e512dd6b19eb27fcbf9dffafb395d831
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-03-24 16:28:01 +0800

    rename a exception class
    PoolInitializedException --> PoolInitializeFailedException

---
+++ src/main/java/org/stone/beecp/pool/exception/PoolInitializeFailedException.java
@@ -0,0 +1,28 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under Apache License v2.0.
+ */
+package org.stone.beecp.pool.exception;
+
+import org.stone.beecp.BeeConnectionPoolException;
+
+/**
+ * pool initialize failed exception
+ *
+ * @author Chris Liao
+ * @version 1.0
+ */
+public class PoolInitializeFailedException extends BeeConnectionPoolException {
+    public PoolInitializeFailedException(String s) {
+        super(s);
+    }
+
+    public PoolInitializeFailedException(Throwable cause) {
+        super(cause);
+    }
+}
