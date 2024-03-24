ab01fe78688f71a28e15bf57ae43ed5c2e657750
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-03-24 15:15:13 +0800

    update testcase file on decoder

--- src/test/java/org/stone/beecp/config/JdbcDecoderExecuteTest.java
+++ src/test/java/org/stone/beecp/config/JdbcDecoderExecuteTest.java
@@ -12,9 +12,13 @@ package org.stone.beecp.config;
 import junit.framework.TestCase;
 import org.stone.base.TestException;
 import org.stone.base.TestUtil;
+import org.stone.beecp.BeeDataSource;
 import org.stone.beecp.BeeDataSourceConfig;
+import org.stone.beecp.RawXaConnectionFactory;
 import org.stone.beecp.config.customization.DummyJdbcLinkInfoDecoder;
 import org.stone.beecp.config.customization.NullConnectionFactory;
+import org.stone.beecp.mock.MockXaDataSource;
+import org.stone.beecp.pool.FastConnectionPool;
 
 import java.util.Properties;
 
@@ -48,9 +52,52 @@ public class JdbcDecoderExecuteTest extends TestCase {
         BeeDataSourceConfig checkedConfig = config.check();
 
         NullConnectionFactory factory = (NullConnectionFactory) TestUtil.getFieldValue(checkedConfig, "connectionFactory");
+
         String url = factory.getUrl();
         String user = factory.getUser();
         String password = factory.getPassword();
+        if (!url.endsWith("-Decoded")) throw new TestException();
+        if (!user.endsWith("-Decoded")) throw new TestException();
+        if (!password.endsWith("-Decoded")) throw new TestException();
+    }
+
+    public void testJdbcDecoderOnXaDataSource() throws Exception {
+        BeeDataSourceConfig config = new BeeDataSourceConfig();
+        config.setJdbcUrl("jdbc:mock:test");
+        config.setUsername("mock");
+        config.setPassword("root");
+        config.setConnectionFactoryClassName("org.stone.beecp.mock.MockXaDataSource");
+        config.setJdbcLinkInfDecoderClassName("org.stone.beecp.config.customization.DummyJdbcLinkInfoDecoder");
+        BeeDataSource ds = new BeeDataSource(config);
+
+        FastConnectionPool pool = (FastConnectionPool) TestUtil.getFieldValue(ds, "pool");
+        RawXaConnectionFactory rawXaConnFactory = (RawXaConnectionFactory) TestUtil.getFieldValue(pool, "rawXaConnFactory");
+        MockXaDataSource xaDs = (MockXaDataSource) TestUtil.getFieldValue(rawXaConnFactory, "dataSource");
+
+        String url = xaDs.getURL();
+        String user = xaDs.getUser();
+        String password = xaDs.getPassword();
+        if (!url.endsWith("-Decoded")) throw new TestException();
+        if (!user.endsWith("-Decoded")) throw new TestException();
+        if (!password.endsWith("-Decoded")) throw new TestException();
+    }
+
+    public void testJdbcDecoderOnXaDataSource2() throws Exception {
+        BeeDataSourceConfig config = new BeeDataSourceConfig();
+        config.setConnectionFactoryClassName("org.stone.beecp.mock.MockXaDataSource");
+        config.setJdbcLinkInfDecoderClassName("org.stone.beecp.config.customization.DummyJdbcLinkInfoDecoder");
+        config.addConnectProperty("URL", "jdbc:mock:test");
+        config.addConnectProperty("user", "mock");
+        config.addConnectProperty("password", "root");
+        BeeDataSource ds = new BeeDataSource(config);
+
+        FastConnectionPool pool = (FastConnectionPool) TestUtil.getFieldValue(ds, "pool");
+        RawXaConnectionFactory rawXaConnFactory = (RawXaConnectionFactory) TestUtil.getFieldValue(pool, "rawXaConnFactory");
+        MockXaDataSource xaDs = (MockXaDataSource) TestUtil.getFieldValue(rawXaConnFactory, "dataSource");
+
+        String url = xaDs.getURL();
+        String user = xaDs.getUser();
+        String password = xaDs.getPassword();
         if (!url.endsWith("-Decoded")) throw new TestException();
         if (!user.endsWith("-Decoded")) throw new TestException();
         if (!password.endsWith("-Decoded")) throw new TestException();
