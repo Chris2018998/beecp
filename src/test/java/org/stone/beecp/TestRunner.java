77a86efcc9794a09f12635d62cf38b2de490db3b
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-04-09 20:55:04 +0800

    restore abandon test runner

--- src/test/java/org/stone/beecp/TestRunner.java
+++ src/test/java/org/stone/beecp/TestRunner.java
@@ -1,21 +1,27 @@
-///*
-// * Copyright(C) Chris2018998
-// * Contact:Chris2018998@tom.com
-// *
-// * Licensed under GNU Lesser General Public License v2.1
-// */
-//package org.stone.beecp;
-//
-//public class TestRunner {
-//    private static final String defaultFilename = "beecp/testCase.properties";
-//
-//    public static void main(String[] ags) throws Throwable {
-//        org.stone.base.TestRunner.main(new String[]{"beecp", defaultFilename});
-//    }
-//
-//    public void testRun() throws Throwable {
-//        long beginTime = System.currentTimeMillis();
-//        org.stone.base.TestRunner.main(new String[]{"beecp", defaultFilename});
-//        System.out.println("Took time:(" + (System.currentTimeMillis() - beginTime) + ")ms");
-//    }
-//}
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright(C) Chris2018998,All rights reserved.
+ *
+ * Project owner contact:Chris2018998@tom.com.
+ *
+ * Project Licensed under Apache License v2.0
+ */
+package org.stone.beecp;
+
+import junit.framework.TestCase;
+import org.stone.base.StoneTestRunner;
+
+public class TestRunner extends TestCase {
+    private static final String defaultFilename = "beecp/testCase.properties";
+
+    public static void main(String[] ags) throws Throwable {
+        StoneTestRunner.main(new String[]{"beecp", defaultFilename});
+    }
+
+    public void testRun() throws Throwable {
+        long beginTime = System.currentTimeMillis();
+        StoneTestRunner.main(new String[]{"beecp", defaultFilename});
+        System.out.println("Took time:(" + (System.currentTimeMillis() - beginTime) + ")ms");
+    }
+}
