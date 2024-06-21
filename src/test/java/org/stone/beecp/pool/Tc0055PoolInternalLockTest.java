d29b354934a79df38366521a33f567a8e352c503
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-06-21 22:40:24 +0800

    upload new test files

---
+++ src/test/java/org/stone/beecp/pool/Tc0055PoolInternalLockTest.java
@@ -0,0 +1,51 @@
+package org.stone.beecp.pool;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.stone.base.TestUtil;
+import org.stone.beecp.BeeDataSourceConfig;
+import org.stone.beecp.config.DsConfigFactory;
+import org.stone.beecp.objects.BorrowThread;
+import org.stone.beecp.objects.MockNetBlockConnectionFactory;
+
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+public class Tc0055PoolInternalLockTest extends TestCase {
+
+    public void testWaitOnPoolLock() throws SQLException {
+        BeeDataSourceConfig config = DsConfigFactory.createDefault();
+        config.setInitialSize(0);
+        config.setMaxActive(2);
+        config.setBorrowSemaphoreSize(2);
+        config.setMaxWait(TimeUnit.SECONDS.toMillis(10));
+        config.setRawConnectionFactory(new MockNetBlockConnectionFactory());
+        FastConnectionPool pool = new FastConnectionPool();
+        pool.init(config);
+
+        BorrowThread first = new BorrowThread(pool);
+        first.start();
+        TestUtil.joinUtilWaiting(first);
+        Assert.assertTrue(pool.getPoolLockHoldTime() > 0L);//first thread has hold lock and blocked in driver
+
+        BorrowThread second = new BorrowThread(pool);
+        second.start();
+        TestUtil.joinUtilWaiting(second);//wait on pool lock
+
+        boolean firstInterrupted = false;
+        boolean secondInterrupted = false;
+        Thread[] threads = pool.interruptOnPoolLock();
+        Assert.assertNotNull(threads);
+        for (Thread thread : threads) {
+            if (thread == first) firstInterrupted = true;
+            else if (thread == second) secondInterrupted = true;
+        }
+
+        try {
+            Assert.assertTrue(firstInterrupted);
+            Assert.assertTrue(secondInterrupted);
+        } finally {
+            pool.close();
+        }
+    }
+}
