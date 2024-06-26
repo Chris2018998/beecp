ded1af8e7306c041bec81899e93f6b79deb2dcbd
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-06-26 21:30:47 +0800

    upload new test files

---
+++ src/test/java/org/stone/beecp/pool/Tc0057ConnectionHoldTest.java
@@ -0,0 +1,83 @@
+package org.stone.beecp.pool;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.stone.base.TestUtil;
+import org.stone.beecp.BeeDataSource;
+import org.stone.beecp.BeeDataSourceConfig;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import static org.stone.beecp.config.DsConfigFactory.createDefault;
+import static org.stone.beecp.pool.ConnectionPoolStatics.oclose;
+
+public class Tc0057ConnectionHoldTest extends TestCase {
+
+    public void testHoldTimeout() throws Exception {
+        BeeDataSourceConfig config = createDefault();
+        config.setInitialSize(0);
+        config.setAliveTestSql("SELECT 1 from dual");
+
+        config.setHoldTimeout(500L);// hold and not using connection;
+        config.setTimerCheckInterval(1000L);// two seconds interval
+        BeeDataSource ds = new BeeDataSource(config);
+        Connection con = null;
+        try {
+            FastConnectionPool pool = (FastConnectionPool) TestUtil.getFieldValue(ds, "pool");
+            con = ds.getConnection();
+            Assert.assertEquals(1, pool.getTotalSize());
+            Assert.assertEquals(1, pool.getUsingSize());
+
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(2));
+            Assert.assertEquals(0, pool.getUsingSize());
+
+            try {
+                con.getCatalog();
+                fail("must throw closed exception");
+            } catch (SQLException e) {
+                Assert.assertTrue(e.getMessage().contains("No operations allowed after connection closed"));
+            }
+
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(2));
+        } finally {
+            if (con != null) oclose(con);
+            ds.close();
+        }
+    }
+
+    public void testHoldNotTimeout() throws Exception {
+        BeeDataSourceConfig config = createDefault();
+        config.setInitialSize(0);
+        config.setAliveTestSql("SELECT 1 from dual");
+        config.setHoldTimeout(0);//not timeout in hold
+
+        config.setTimerCheckInterval(1000L);// two seconds interval
+        BeeDataSource ds = new BeeDataSource(config);
+        Connection con = null;
+        try {
+            FastConnectionPool pool = (FastConnectionPool) TestUtil.getFieldValue(ds, "pool");
+            con = ds.getConnection();
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));//first sleeping
+
+            Assert.assertEquals(1, pool.getTotalSize());
+            Assert.assertEquals(1, pool.getUsingSize());
+
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));//second sleeping
+
+            Assert.assertEquals(1, pool.getTotalSize());
+            Assert.assertEquals(1, pool.getUsingSize());
+
+            try {
+                con.getCatalog();
+            } catch (SQLException e) {
+                Assert.assertEquals(e.getMessage(), "Connection has been recycled by force");
+            }
+        } finally {
+            if (con != null) oclose(con);
+            ds.close();
+        }
+    }
+}
---
+++ src/test/java/org/stone/beecp/pool/Tc0058ConnectionIdleTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright(C) Chris2018998
+ * Contact:Chris2018998@tom.com
+ *
+ * Licensed under GNU Lesser General Public License v2.1
+ */
+package org.stone.beecp.pool;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.stone.base.TestUtil;
+import org.stone.beecp.BeeDataSource;
+import org.stone.beecp.BeeDataSourceConfig;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import static org.stone.beecp.config.DsConfigFactory.createDefault;
+
+public class Tc0058ConnectionIdleTest extends TestCase {
+
+    public void testIdleTimeout() throws Exception {
+        final int initSize = 5;
+        BeeDataSourceConfig config = createDefault();
+        config.setInitialSize(initSize);
+        config.setMaxActive(initSize);
+        config.setAliveTestSql("SELECT 1 from dual");
+        config.setIdleTimeout(1000);
+        config.setTimerCheckInterval(1000);
+        BeeDataSource ds = new BeeDataSource(config);
+        try {
+            FastConnectionPool pool = (FastConnectionPool) TestUtil.getFieldValue(ds, "pool");
+
+            Assert.assertEquals(initSize, pool.getTotalSize());
+            Assert.assertEquals(initSize, pool.getIdleSize());
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(2));
+
+            Assert.assertEquals(0, pool.getTotalSize());
+            Assert.assertEquals(0, pool.getIdleSize());
+        } finally {
+            ds.close();
+        }
+    }
+}
