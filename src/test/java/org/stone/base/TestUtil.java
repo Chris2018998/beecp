9a33599a751d3f93ac7b1f1834d5ea82557bff34
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-07-03 23:24:54 +0800

    sync source code from stone project

--- src/test/java/org/stone/base/TestUtil.java
+++ src/test/java/org/stone/base/TestUtil.java
@@ -11,22 +11,32 @@ package org.stone.base;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.Appender;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.stone.tools.CommonUtil;
 
+import javax.sql.XAConnection;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 import static org.stone.tools.BeanUtil.setAccessible;
+import static org.stone.tools.CommonUtil.objectEquals;
 
 /**
  * @author Chris Liao
  * @version 1.0
  */
 public class TestUtil {
+    public static final long Wait_Time = 100L;
+    public static final TimeUnit Wait_TimeUnit = TimeUnit.MILLISECONDS;
+    private static final Logger log = LoggerFactory.getLogger(TestUtil.class);
     private static StoneLogAppender logAppender = null;
 
     public static StoneLogAppender getStoneLogAppender() {
@@ -58,14 +68,105 @@ public class TestUtil {
         return field.get(ob);
     }
 
-    public static Object invokeMethod(final Object ob, final String methodName) throws Exception {
-        return invokeMethod(ob, ob.getClass(), methodName);
+    public static Object invokeMethod2(final Object ob, final String methodName) throws Exception {
+        return invokeMethod2(ob, ob.getClass(), methodName);
     }
 
-    public static Object invokeMethod(final Object ob, final Class clazz, final String methodName) throws Exception {
+    public static Object invokeMethod2(final Object ob, final Class clazz, final String methodName) throws Exception {
         Method method = clazz.getDeclaredMethod(methodName);
         setAccessible(method);
         return method.invoke(ob);
+    }
+
+    public static Object invokeMethod(Object ob, String methodName) {
+        try {
+            Method method = ob.getClass().getMethod(methodName);
+            method.setAccessible(true);
+            return method.invoke(ob);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void assertError(String message) {
+        throw new AssertionError(message);
+    }
+
+    public static void assertError(String message, Object expect, Object current) {
+        if (!objectEquals(expect, current))
+            throw new AssertionError(String.format(message, expect, current));
+    }
+
+    public static boolean containsMessage(Throwable e, String msg) {
+        String errorInfo = e.getMessage();
+        return errorInfo != null && errorInfo.contains(msg);
+    }
+
+    public static void setFieldValue(Object ob, String fieldName, Object value) throws Exception {
+        Field field = ob.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(ob, value);
+    }
+
+//    public static Object getFieldValue(Object ob, String fieldName) throws Exception {
+//        Field field = ob.getClass().getDeclaredField(fieldName);
+//        field.setAccessible(true);
+//        return field.get(ob);
+//    }
+//
+//    public static Object invokeMethod(Object ob, String methodName) {
+//        try {
+//            Method method = ob.getClass().getMethod(methodName);
+//            method.setAccessible(true);
+//            return method.invoke(ob);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+
+    public static void oclose(ResultSet r) {
+        try {
+            r.close();
+        } catch (Throwable e) {
+            log.warn("Warning:Error at closing resultSet:", e);
+        }
+    }
+
+    public static void oclose(Statement s) {
+        try {
+            s.close();
+        } catch (Throwable e) {
+            log.warn("Warning:Error at closing statement:", e);
+        }
+    }
+
+    public static void oclose(Connection c) {
+        try {
+            c.close();
+        } catch (Throwable e) {
+            log.warn("Warning:Error at closing connection:", e);
+        }
+    }
+
+    public static void oclose(XAConnection c) {
+        try {
+            c.close();
+        } catch (Throwable e) {
+            log.warn("Warning:Error at closing resultSet:", e);
+        }
+    }
+
+    public static boolean joinUtilTerminated(Thread thread) {
+        for (; ; ) {
+            Thread.State curState = thread.getState();
+            if (curState == Thread.State.TERMINATED) {
+                return true;
+            } else {
+                LockSupport.parkNanos(5L);
+            }
+        }
     }
 
     public static boolean joinUtilWaiting(Thread thread) {
