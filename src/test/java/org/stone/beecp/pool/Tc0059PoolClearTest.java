b63133bace8af5d284b1e3badeded78d62bb59cd
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2024-07-06 07:58:51 +0800

    add new test case for pool clear

--- src/test/java/org/stone/beecp/pool/Tc0059PoolClearTest.java
+++ src/test/java/org/stone/beecp/pool/Tc0059PoolClearTest.java
@@ -4,8 +4,12 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.stone.base.StoneLogAppender;
 import org.stone.beecp.BeeDataSourceConfig;
+import org.stone.beecp.BeeDataSourceConfigException;
+import org.stone.beecp.objects.MockCreateExceptionConnectionFactory;
+import org.stone.beecp.pool.exception.PoolInitializeFailedException;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
@@ -14,7 +18,7 @@ import static org.stone.beecp.config.DsConfigFactory.createDefault;
 
 public class Tc0059PoolClearTest extends TestCase {
 
-    public void testClearAllIdles() throws Exception {
+    public void testClearAllIdles() throws SQLException {
         BeeDataSourceConfig config = createDefault();
         config.setInitialSize(2);
         config.setMaxActive(2);
@@ -39,7 +43,7 @@ public class Tc0059PoolClearTest extends TestCase {
         Assert.assertTrue(logs.isEmpty());
     }
 
-    public void testForceClearUsings() throws Exception {
+    public void testForceClearUsings() throws SQLException {
         BeeDataSourceConfig config = createDefault();
         config.setInitialSize(1);
         config.setMaxActive(1);
@@ -53,7 +57,7 @@ public class Tc0059PoolClearTest extends TestCase {
         Assert.assertEquals(0, pool.getTotalSize());
     }
 
-    public void testClearHoldTimeout() throws Exception {//manual clear hold timeout connections
+    public void testClearHoldTimeout() throws SQLException {//manual clear hold timeout connections
         BeeDataSourceConfig config = createDefault();
         config.setInitialSize(1);
         config.setMaxActive(1);
@@ -69,7 +73,7 @@ public class Tc0059PoolClearTest extends TestCase {
         Assert.assertEquals(0, pool.getTotalSize());
     }
 
-    public void testClearUsingOnReturn() throws Exception {//manual clear hold timeout connections
+    public void testClearUsingOnReturn() throws SQLException {//manual clear hold timeout connections
         BeeDataSourceConfig config = createDefault();
         config.setInitialSize(1);
         config.setMaxActive(1);
@@ -83,6 +87,61 @@ public class Tc0059PoolClearTest extends TestCase {
         pool.clear(false);
         Assert.assertEquals(0, pool.getUsingSize());
         Assert.assertEquals(0, pool.getTotalSize());
+    }
+
+    public void testClearAndStartupPool() throws SQLException {
+        BeeDataSourceConfig config1 = createDefault();
+        config1.setInitialSize(5);
+        config1.setMaxActive(5);
+        FastConnectionPool pool = new FastConnectionPool();
+        pool.init(config1);
+        Assert.assertEquals(5, pool.getTotalSize());
+
+        BeeDataSourceConfig config2 = createDefault();
+        config2.setInitialSize(10);
+        config2.setMaxActive(10);
+        pool.clear(false, config2);
+        Assert.assertEquals(10, pool.getTotalSize());
+    }
+
+    public void testPoolRestart() throws SQLException {
+        BeeDataSourceConfig config1 = createDefault();
+        config1.setInitialSize(2);
+        config1.setMaxActive(2);
+        FastConnectionPool pool = new FastConnectionPool();
+        pool.init(config1);
+        Assert.assertEquals(2, pool.getTotalSize());
+
+        BeeDataSourceConfig config2 = createDefault();
+        config2.setMaxActive(5);
+        config2.setInitialSize(10);
+        try {
+            pool.clear(false, config2);
+            fail("failed test clear");
+        } catch (PoolInitializeFailedException e) {
+            Throwable cause = e.getCause();
+            Assert.assertTrue(cause instanceof BeeDataSourceConfigException);
+            Assert.assertEquals("initialSize must not be greater than maxActive", cause.getMessage());
+
+            config2.setMaxActive(10);
+            config2.setInitialSize(10);
+            pool.clear(false, config2);
+            Assert.assertEquals(10, pool.getTotalSize());
+        }
+
+        BeeDataSourceConfig config3 = createDefault();
+        config3.setInitialSize(1);
+        config3.setRawConnectionFactory(new MockCreateExceptionConnectionFactory());
+        try {
+            pool.clear(false, config3);
+            fail("failed test clear");
+        } catch (SQLException e) {
+            Assert.assertEquals("Network communications error", e.getMessage());
+            config3.setRawConnectionFactory(null);
+
+            pool.clear(false, config3);
+            Assert.assertEquals(1, pool.getTotalSize());
+        }
     }
 
     private final static class DelayCloseThread extends Thread {
